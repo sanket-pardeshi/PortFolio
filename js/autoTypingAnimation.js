cb96c8fbd5688ff6c1fbd1dd4b9c259932244a39
sanket-pardeshi <sanketpardeshi28702@gmail.com>
2023-10-21 17:44:39 +0530

    Initial commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Sanket Pardeshi</span> currently pursuing my B.Tech in Computer Engineering from Vishwakarma Institute of Information Technology, Pune. With a relentless passion for technology and a drive for excellence, I am on a mission to make a meaningful impact in the field.<br><br> Throughout my academic journey, I have been immersed in a rigorous curriculum that has equipped me with a strong foundation in programming, web development, and app development. I am constantly honing my skills and staying updated with the latest industry trends to ensure I can deliver top-notch solutions. <br><br>I believe in the power of collaboration and effective communication, and I thrive in team environments. I am eager to contribute my skills, creativity, and problem-solving abilities to drive innovation within organizations. Let's connect and explore how my dedication, technical expertise, and enthusiasm can contribute to the success of your team."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
